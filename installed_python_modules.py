dc8b082e199b28d12e3ce7984b5c4f584baeae5b
topher235 <topherhurt@gmail.com>
2016-07-09 07:28:51 -0400

    Fixed: Opens file in txt editor

--- installed_python_modules.py
+++ installed_python_modules.py
@@ -1,7 +1,19 @@
 import pip, io, datetime
+import os, sys, subprocess
 
 #Recieve list of installed packages
 installed_packages = pip.get_installed_distributions()
+
+#Opens the text file containing the python modules
+def open_file(filename):
+	if(sys.platform == "win32"):
+		os.startfile(filename)
+	else:
+		if(sys.platform == "darwin"):
+			opener = "open"
+		else:
+			opener = "xdg-open"
+		subprocess.call([opener, filename])
 
 #Write the list of packages to file: installed_python_modules
 try:
@@ -12,8 +24,11 @@ try:
 			file.write('{:>{space}}'.format("Version: " + i.version + '\n', space=space))
 		today = datetime.date.today().isoformat()
 		file.write("Date: "  + today)
-
+		file.close()
+		open_file('installed_python_modules')
 except IOError:
 	print("ERROR -- Could not open file")
+
+
 
 #TODO: Fix formatting
